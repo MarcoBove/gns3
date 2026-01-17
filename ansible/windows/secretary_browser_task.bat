c24aedea3cfbafade73231e7263e12b28ebb3f52
Marco Bove <149075171+MarcoBove@users.noreply.github.com>
2026-01-17 22:49:06 +0100

    Update secretary_browser_task.bat

--- ansible/windows/secretary_browser_task.bat
+++ ansible/windows/secretary_browser_task.bat
@@ -7,8 +7,21 @@ if NOT "%~1"=="" goto :MODE_RECEIVE
 goto :MODE_EXECUTE
 
 :MODE_RECEIVE
-set "INPUT_TARGET=%~1"
-set "ACTION=%~2"
+set "P1=%~1"
+set "P2=%~2"
+set "P3=%~3"
+
+:: Se esiste un terzo parametro, significa che Windows ha spezzato l'URL sull'uguale (=)
+:: P1 = prima parte URL, P2 = ID video, P3 = Azione (video)
+if NOT "%P3%"=="" (
+    set "INPUT_TARGET=%P1%=%P2%"
+    set "ACTION=%P3%"
+) else (
+    :: Nessuno split, tutto normale
+    set "INPUT_TARGET=%P1%"
+    set "ACTION=%P2%"
+)
+
 if "%ACTION%"=="" set ACTION=generic
 
 :: Scriviamo nel file
